adb3ffbdc6ef6f8068954c439b239341ac6b77d5
Ahmed Magid <devahmedmagid@gmail.com>
2022-07-28 21:15:32 +0200

    first commit

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIjWMGIjEXOVXiwWbWrAPZvxcyxLWrloU',
+  authDomain: 'comerca-12fe0.firebaseapp.com',
+  projectId: 'comerca-12fe0',
+  storageBucket: 'comerca-12fe0.appspot.com',
+  messagingSenderId: '639651344636',
+  appId: '1:639651344636:web:704961928825d5d0912779',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ readme.md
@@ -0,0 +1,1 @@
+comerca app
